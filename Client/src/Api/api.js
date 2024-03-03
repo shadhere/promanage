c631fbc1603367b6166823c9fc50b5db85b1b26c
shadhere <93948067+shadhere@users.noreply.github.com>
2024-03-03 21:55:17 +0530

    done

--- Client/src/Api/api.js
+++ Client/src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://myprojectpromage.vercel.app/",
+  baseURL: "https://promanage-1-5mjl.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
--- Client/src/Components/ModernCard/ModernCard.jsx
+++ Client/src/Components/ModernCard/ModernCard.jsx
@@ -100,7 +100,7 @@ const ModernCard = ({
 
   const handleShare = () => {
     if (carrdId) {
-      const cardLink = `https://promanage-rycy.vercel.app/tasks/${carrdId}
+      const cardLink = `https://promanage-1-5mjl.onrender.com//tasks/${carrdId}
       `; // Construct the link with the dynamic ID
       navigator.clipboard
         .writeText(cardLink)
