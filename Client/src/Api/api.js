1edc4b1a5e9e41e2a3600017c9da527284b41b47
shadhere <93948067+shadhere@users.noreply.github.com>
2024-03-03 19:43:46 +0530

    ADDEd

--- Client/src/Api/api.js
+++ Client/src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://promanage-rycy.vercel.app/",
+  baseURL: "https://myprojectpromage.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
